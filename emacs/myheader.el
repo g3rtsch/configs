b3860a4395cc8cc17994f98a5ec63235ff97bc0e
Gertsch <chellomailer@gertsch.cc>
2017-04-19 21:05:09 +0200

    myheader adjusted

--- emacs/myheader.el
+++ emacs/myheader.el
@@ -5,15 +5,35 @@
 (load-relative "header2.el")
 (require 'header2)
 
-(defsubst my/header-mode-line ()
-  "Insert !/bin/bash"
-  (insert header-prefix-string "!/bin/bash\n"))
+(defsubst auto-make-elisp-header ()
+;; (defun auto-make-elisp-header ()
+  "Make headers for Lisp files."
+  (setq make-header-hook '(header-title
+			   header-blank
+			   header-file-name
+			   header-description
+			   header-author
+			   header-creation-date
+			   header-commentary
+			   header-blank
+			   header-blank
+			   header-end-line
+			   header-code
+			   header-eof))
+  (auto-make-header))
 
-(setq make-header-hook '(header-shell
-			 header-creation-date
-			 header-author))
+(defsubst auto-make-shell-header ()
+;; (defun auto-make-shell-header ()
+  "Make headers for shell files."
+  (setq make-header-hook '(header-shell
+			   header-creation-date
+			   header-author))
+  (auto-make-header))
 
-(add-hook 'sh-mode-hook 'auto-make-header)
+(autoload 'auto-make-header "header2")
+(add-hook 'emacs-lisp-mode-hook 'auto-make-elisp-header)
+(add-hook 'sh-mode-hook 'auto-make-shell-header)
+
 
 (provide 'myheader)
 ;;; myheader.el ends here
